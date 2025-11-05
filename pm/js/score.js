c1d712830be638046cd8f2c4d2c54d5bdab3f7a1
sminggs <smins1063@gmail.com>
2025-11-05 16:52:18 +0900

    feat: pc 완료

--- pm/js/score.js
+++ pm/js/score.js
@@ -1,16 +1,16 @@
 // 점수 화면 애니메이션
 
 $(document).ready(function() {
-    // 페이지 로드 후 점수 바 애니메이션
-    setTimeout(function() {
-        $('.score-bar').each(function(index) {
-            const $bar = $(this);
-            const width = $bar.data('width');
-            
-            setTimeout(function() {
-                $bar.find('.score-bar-fill').css('width', width + '%');
-            }, index * 100);
-        });
-    }, 300);
+		// 페이지 로드 후 점수 바 애니메이션
+		setTimeout(function() {
+				$('.score-bar').each(function(index) {
+						const $bar = $(this);
+						const width = $bar.data('width');
+						
+						setTimeout(function() {
+								$bar.find('.score-bar-fill').css('width', width + '%');
+						}, index * 100);
+				});
+		}, 300);
 });
 
